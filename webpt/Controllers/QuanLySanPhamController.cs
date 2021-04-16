02e896f77a58a4ad2c3539bf22f02cb5e678cafc
lu <lulv.lukai2212@gmail.com>
2021-04-16 18:47:01 +0700

    first commit

---
+++ webpt/Controllers/QuanLySanPhamController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webpt.Models;
+
+namespace webpt.Controllers
+{
+    public class QuanLySanPhamController : Controller
+    {
+        // GET: QuanLySanPham
+        BanPhuTungXeEntities db = new BanPhuTungXeEntities();
+        public ActionResult Index()
+        {
+            return View(db.SanPhams.ToList());
+        }
+    }
+}
